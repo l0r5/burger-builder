9381ae9028cafbc9440a425f25f19c7cd2eab08d
l0r5 <lars-braitinger@web.de>
2021-04-08 18:59:22 +0200

    Output the orders depending on the data from the server

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -9,28 +9,33 @@ class Orders extends Component {
         orders: [],
         loading: true
     }
+
     componentDidMount() {
         axios.get('/orders.json')
             .then(res => {
                 const fetchedOrders = [];
-                for(let key in res.data) {
+                for (let key in res.data) {
                     fetchedOrders.push({
                         ...res.data[key],
                         id: key
                     });
                 }
-                this.setState({loading: false});
+                this.setState({loading: false, orders: fetchedOrders});
             })
-            .catch(err=>{
+            .catch(err => {
                 this.setState({loading: false});
             });
     }
 
     render() {
-        return(
+        return (
             <div>
-                <Order />
-                <Order />
+                {this.state.orders.map(order => (
+                    <Order
+                        key={order.id}
+                        ingredients={order.ingredients}
+                        price={+order.price}/>
+                ))}
             </div>
         );
     }
